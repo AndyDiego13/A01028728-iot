4daf0082403897c56fa8d07d789c34af9f07ebe1
AndyDiego13 <andyserrano_d@outlook.com>
2020-11-05 12:16:34 -0600

    nop

---
+++ Code/ser.py
@@ -0,0 +1,14 @@
+#fd = open("hola.txt","w")
+#fd.readline()
+#for line in fd:
+    #print(line)
+#a se salta porque es la linea 1
+#b
+#c
+
+import serial
+
+ser = serial.Serial(puerto,9600 bits*seg)
+while(1):
+    value = ser.readline()
+    print(value)
